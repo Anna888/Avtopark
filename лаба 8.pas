1f35a7508998b8488b2d7223a1d6e6729aafaabf
Anna888 <Anna5Pavel>
2015-05-24 15:32:28 +0300

    Кинула на веточку лабу

---
+++ лаба 8.pas
@@ -0,0 +1,27 @@
+{1.	Четырехугольник задан своими сторонами a,b,c,d и диагональю r. 
+Вычислить площадь четырехугольника с использованием функции нахождения площади треугольника.}
+
+
+uses
+  crt;
+
+var
+  a, b, c, d, r,  S: real;
+
+function f(x, y, z: real): real;
+var
+  p: real;
+begin
+  p := (x + y + z) / 2;
+  result := Sqrt(p * (p - x) * (p - y) * (p - z));
+end;
+
+begin
+  Writeln('введите 4 стороны и диагональ четырехугольника');
+  readln(a, b, c, d, r);
+   
+  s := f(a, b, r)+f(r, c, d);
+  writeln(S:4:2);
+  readln;
+   
+end.
